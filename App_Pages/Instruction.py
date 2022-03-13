ff2fd19253b20d0469614dc9bbe74f37b9e469e8
markrjmarte <61866516+markrjmarte@users.noreply.github.com>
2022-03-13 21:29:05 +0800

    Update Instruction.py

--- App_Pages/Instruction.py
+++ App_Pages/Instruction.py
@@ -37,8 +37,8 @@ def app():
             </div>
         </body>
         """,unsafe_allow_html=True)
-        st.image('App Images/I1.png',use_column_width=True)
-        st.markdown("""
+    st.image('App Images\I1.png',use_column_width=True)
+    st.markdown("""
         <style>
             .Paragraph {
                 background-color: #e1ffca;
@@ -58,8 +58,8 @@ def app():
             </div>
         </body>
         """,unsafe_allow_html=True)
-        st.image('App Images/I2.png',use_column_width=True)
-        st.markdown("""
+    st.image('App Images\I2.png',use_column_width=True)
+    st.markdown("""
         <style>
             .Paragraph {
                 background-color: #e1ffca;
@@ -79,8 +79,8 @@ def app():
             </div>
         </body>
         """,unsafe_allow_html=True)
-        st.image('App Images/I3.png',use_column_width=True)
-        st.markdown("""
+    st.image('App Images\I3.png',use_column_width=True)
+    st.markdown("""
         <style>
             .Paragraph {
                 background-color: #e1ffca;
@@ -100,8 +100,8 @@ def app():
             </div>
         </body>
         """,unsafe_allow_html=True)
-        st.image('App Images/I4.png',use_column_width=True)
-        st.markdown("""
+    st.image('App Images\I4.png',use_column_width=True)
+    st.markdown("""
         <style>
             .Paragraph {
                 background-color: #e1ffca;
@@ -121,12 +121,12 @@ def app():
             </div>
         </body>
         """,unsafe_allow_html=True)
-        st.image('App Images/I5.png',use_column_width=True)
-        st.text('')
-        st.text('')
-        st.text('')
-        st.text('')
-        st.markdown("""
+    st.image('App Images\I5.png',use_column_width=True)
+    st.text('')
+    st.text('')
+    st.text('')
+    st.text('')
+    st.markdown("""
         <style>
             .topnav {
                 background-color: #0ed145;
@@ -162,8 +162,8 @@ def app():
         </body>
         """,unsafe_allow_html=True)
 
-        st.image('App Images/I6.png',use_column_width=True)
-        st.markdown("""
+    st.image('App Images\I6.png',use_column_width=True)
+    st.markdown("""
             <style>
             .Paragraph {
                 background-color: #e1ffca;
@@ -183,8 +183,8 @@ def app():
             </div>
             </body>
             """,unsafe_allow_html=True)
-        st.image('App Images/I7.png',use_column_width=True)
-        st.markdown("""
+    st.image('App Images\I7.png',use_column_width=True)
+    st.markdown("""
         <style>
             .Paragraph {
                 background-color: #e1ffca;
@@ -204,8 +204,8 @@ def app():
             </div>
         </body>
         """,unsafe_allow_html=True)
-        st.image('App Images/I8.png',use_column_width=True)
-        st.markdown("""
+    st.image('App Images\I8.png',use_column_width=True)
+    st.markdown("""
         <style>
             .Paragraph {
                 background-color: #e1ffca;
@@ -225,7 +225,7 @@ def app():
             </div>
         </body>
         """,unsafe_allow_html=True)
-        st.image('App Images/I9.png',use_column_width=True)
+    st.image('App Images\I9.png',use_column_width=True)
     
     
 
